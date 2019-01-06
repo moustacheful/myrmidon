5fa18cbaec39086535bf297d1d6e32f5e93ad122
Daniel Acuña <dacunaavalos@gmail.com>
2019-01-05 23:06:15 -0300

    Commented file, add task checking and optional custom path for tasks

--- myrmidon.sh
+++ myrmidon.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 cwd=$(echo $(dirname $0))
-tasks=$(cat $HOME/.myrmidon-tasks.json)
 
-selected=$(echo $tasks | jq -j 'map(.name) | join("\n")' | rofi -dmenu -matching fuzzy -i -p "Search tasks: ")
+# Use ~/.myrmidon-tasks.json as default, otherwise use incoming path
+config_file="${1:-"$HOME/.myrmidon-tasks.json"}"
+tasks=$(cat $config_file)
+
+# Pass tasks to rofi, and get the output as the selected option
+selected=$(echo $tasks | jq -j 'map(.name) | join("\n")' | rofi -dmenu -matching fuzzy -i -p "Search tasks")
 task=$(echo $tasks | jq ".[] | select(.name == \"$selected\")")
+
+# Exit if no task was found
+if [[ $task == "" ]]; then
+  echo "No task defined as '$selected' within config file."
+  exit 1
+fi
+
 task_command=$(echo $task | jq ".command")
 confirm=$(echo $task | jq ".confirm")
-confirm_script="$cwd/confirm.sh 'Confirm $selected?'"
 
-if [ $confirm == "true" ]; then
+# Check whether we need confirmation to run this task
+if [[ $confirm == "true" ]]; then
+  # Chain the confirm command before executing the selected command
+  confirm_script="$cwd/confirm.sh 'Confirm $selected?'"
   eval "$confirm_script && \"$task_command\" > /dev/null &"
 else
   eval "\"$task_command\" > /dev/null &"
