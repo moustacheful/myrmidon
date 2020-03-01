228ba3d5aedda312f8647e01bc05fc2830b9813b
Pierre-Marie Achart <pierremarie.achart@gmail.com>
2020-03-01 18:31:14 +0100

    more refacto & more new things !

--- myrmidon.sh
+++ myrmidon.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
 function func_rofi_confirm() {
-  local OPTIONS UNSAFE RESPONSE
+  local MESSAGE INVERT YES NO OPTIONS RESPONSE
 
-  [[ ${MESSAGE} == "null" ]] && MESSAGE="Confirm '${SELECTED}' ?"
-  OPTIONS="No\\nYes";
+  MESSAGE="Confirm ${SELECTED} ?"
+  MESSAGE=$(echo "${TASK}" | jq --exit-status --raw-output '.confirm.message | select (.!=null)' || echo "${MESSAGE}")
+  INVERT=$(echo "${TASK}" | jq -e '.confirm.invert')
+  YES=$(echo "${TASK}" | jq -er '.confirm.yes | select (.!=null)' || echo 'Yes')
+  NO=$(echo "${TASK}" | jq -er '.confirm.no | select (.!=null)' || echo 'No')
 
-  UNSAFE=$(echo "${TASK}" | jq ".unsafe")
-  [[ ${UNSAFE} == "true" ]] && OPTIONS="Yes\\nNo";
+  OPTIONS="${NO}\\n${YES}";
+  [[ ${INVERT} == "true" ]] && OPTIONS="${YES}\\n${NO}";
 
-  RESPONSE=$(echo -e "${OPTIONS}" | rofi -dmenu -i -p "${MESSAGE} ")
+  RESPONSE=$(echo -e "${OPTIONS}" | rofi -theme-str '#prompt { enabled: false; }' -dmenu -i -p "${MESSAGE} ")
 
-  if [ "${RESPONSE}" == "Yes" ]; then
+  if [ "${RESPONSE}" == "${YES}" ]; then
     exit 0;
   else
     exit 1;
@@ -19,7 +22,7 @@ function func_rofi_confirm() {
 }
 
 function myrmidon() {
-  local CONFIG_FILE TASKS TASK SELECTED TASK_COMMAND CONFIRM CONFIRM_SCRIPT MESSAGE
+  local CONFIG_FILE TASKS SELECTED TASK TASK_COMMAND CONFIRM
 
   # Use ~/.myrmidon-tasks.json as default, otherwise use incoming path
   CONFIG_FILE="${1:-"${HOME}/.myrmidon-tasks.json"}"
@@ -27,26 +30,25 @@ function myrmidon() {
 
   # Pass tasks to rofi, and get the output as the selected option
   SELECTED=$(echo "${TASKS}" | jq -j 'map(.name) | join("\n")' | rofi -dmenu -matching fuzzy -i -p "Search tasks")
+  [[ ${SELECTED} == '' ]] && exit 0
+
   TASK=$(echo "${TASKS}" | jq ".[] | select(.name == \"${SELECTED}\")")
 
   # Exit if no task was found
-  if [[ ${TASK} == "" ]]; then
+  if [[ ${TASK} == '' ]]; then
     echo "No task defined as '${SELECTED}' within config file."
     exit 1
   fi
 
-  TASK_COMMAND=$(echo "${TASK}" | jq ".command")
-  CONFIRM=$(echo "${TASK}" | jq ".confirm")
-  MESSAGE=$(echo "${TASK}" | jq --raw-output ".message")
+  TASK_COMMAND=$(echo "${TASK}" | jq '.command')
+  CONFIRM=$(echo "${TASK}" | jq '.confirm')
 
   # Check whether we need confirmation to run this task
-  if [[ ${CONFIRM} == "true" ]]; then
-    # Chain the confirm command before executing the selected command
-    CONFIRM_SCRIPT="func_rofi_confirm '${MESSAGE}'"
-    eval "${CONFIRM_SCRIPT} && \"${TASK_COMMAND}\" > /dev/null &"
-  else
-    eval "\"${TASK_COMMAND}\" > /dev/null &"
+  if [[ ${CONFIRM} != 'null' ]]; then
+    func_rofi_confirm || exit 1
   fi
+
+  eval "\"${TASK_COMMAND}\" > /dev/null &"
 }
 
 myrmidon "${@}"
